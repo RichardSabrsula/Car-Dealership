1cbad712d6a30b6aa3207c0b8d0705a04c03c83d
Richard Sabrsula <Richard@Sabrsula.com>
2015-06-01 04:19:50 -0500

    Simulates an in-house car dealership's software

---
+++ assign4/BusyThread.java
@@ -0,0 +1,32 @@
+package assign4;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author vangelis
+ */
+public class BusyThread extends Thread {
+    private boolean busy = true;
+
+    public boolean isBusy() {
+        return busy;
+    }
+
+    public void setBusy(boolean busy) {
+        this.busy = busy;
+    }
+    
+    public void run() {
+        System.out.print("\nWorking.");
+        while (busy) {
+            try {
+                System.out.print(".");
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(BusyThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        System.out.println();
+    }
+}
---
+++ assign4/ConsoleUserInterface.java
@@ -0,0 +1,27 @@
+/** Made by Richard Sabrsula
+*	Simplified console interface to run other views
+*/
+package assign4;
+
+import java.util.Scanner;
+
+/**
+ * Abstract class that represents a Console User Interface.
+ */
+public abstract class ConsoleUserInterface implements Runnable {
+
+    protected static Dealership dealership;
+    protected static Scanner sc = new Scanner(System.in);
+
+    /**
+     * Abstract method. To be implemented by subclasses.
+     */
+    protected abstract void printMenu();
+    
+
+    /**
+     * Abstract method. To be implemented by subclasses.
+     */
+    //public abstract void run();
+
+}
